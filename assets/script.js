bba312b238b55e9bdb51a67e9748b96e0f3eb1c0
Stanis J <stanislav.jilnicov@gmil.com>
2020-12-20 12:18:30 +0200

    1. Deleted alert from function. 2. Deleted useless lines of code

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,29 @@
-function padString(someText, stringLength, symbol, flag) {
-  var result = '';
-
-  if (symbol.length > 1) {
-    alert('Oops... Third parameter must be a single symbol');
-  } else {
-    if (someText.length < stringLength) {
-      if (flag) {
-        result = someText;
-  
-        for (i = 1; i <= (stringLength - someText.length); i++) {
-          result += symbol;
-        }
-      } else {
-        for (i = 1; i <= (stringLength - someText.length); i++) {
-          result += symbol;
-        }
-  
-        result += someText;
-      }
-    } else {
-      result = someText;
-    }
-  
-    return result;
-  }
+function checkSymbol(symbol, symbolAmount) {
+  return symbol.length === symbolAmount;
 }
 
-var functionResult = padString('Come On', 12, '*', true);
+function padString(someText, stringLength, symbol, flag, checker) {
+  var addSymbol = '';
 
-alert(functionResult);
+  if (checker(symbol)) {
+    if (someText.length < stringLength) {
+      for (i = 1; i <= (stringLength - someText.length); i++) {
+        addSymbol += symbol;
+      }
 
+      if (flag) {
+        return someText + addSymbol;
+      } else {
+        return addSymbol + someText;
+      }
+    } else {
+      return someText;
+    }
+  }
+
+  return 'Oops... Third parameter must be a single symbol';
+}
+
+var result = padString('Hillel Forever', 20, '*', true, function(a) {return checkSymbol(a, 1)});
+
+alert(result);
